38999ac15405265d217d1be106f86a53d3feeac0
Fahmousss <09031382227177>
2025-06-28 19:39:05 +0700

    feat: add active scope to Category model for filtering active categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -45,5 +47,11 @@ class Category extends Model
     public function songkets(): HasMany
     {
         return $this->hasMany(Songket::class);
+    }
+
+    #[Scope]
+    protected function active(Builder $query): void
+    {
+        $query->where('is_active', true);
     }
 }
